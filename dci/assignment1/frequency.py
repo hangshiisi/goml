943a59510696aaeab5f4bef073c774755dc21664
Hang Shi <hangshiisi@gmail.com>
2017-09-22 23:38:37 -0700

    states

---
+++ dci/assignment1/frequency.py
@@ -0,0 +1,23 @@
+from __future__ import division
+import sys
+import json
+from collections import Counter
+
+
+def frequency(tweet_file):
+    "string -> dict"
+    cnt = Counter() 
+    with open(tweet_file) as f:
+        tweets = (json.loads(line).get('text', '').split() for line in f)
+        #return Counter(word for tweet in tweets for word in tweet)
+        for tweet in tweets:
+            for word in tweet: 
+                cnt[word] += 1  
+    return cnt 
+
+if __name__ == '__main__':
+    frecuencies = frequency(tweet_file=sys.argv[1])
+    total = sum(frecuencies.values())
+    sys.stdout.writelines('{0} {1}\n'.format(word.encode('utf-8'), frecuencies[word] / total) for word in frecuencies)
+
+
--- dci/assignment1/term_sentiment.py
+++ dci/assignment1/term_sentiment.py
@@ -1,17 +1,37 @@
+from __future__ import division
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def read_scores(sent_file):
+    '''parse sentiment file, returns dictionary''' 
+    scores = {} 
+    with open(sent_file) as fp: 
+        for line in fp: 
+            term, score = line.split("\t") 
+            scores[term] = int(score) 
+        return scores 
 
-def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+def score_tweet(tweet, scores):
+    '''returns the score for a tweet or 0 if it's not in file''' 
+    return sum(scores.get(word, 0) for word in tweet)
+
+
+def unknown_word_scores(tweet_file, scores):
+    with open(tweet_file) as f:
+        tweets = (json.loads(line).get('text', '').split() for line in f)
+        return {word: score_tweet(tweet, scores) / len(tweet)
+                for tweet in tweets if tweet
+                for word in tweet if word not in scores}
+
+def main(): 
+    scores = read_scores(sent_file=sys.argv[1])
+    sys.stdout.writelines('{0} {1}\n'.format(word.encode('utf-8'), score)
+                          for word, score in unknown_word_scores(
+                              tweet_file=sys.argv[2],
+                              scores=scores).items())
+
 
 if __name__ == '__main__':
     main()
+
