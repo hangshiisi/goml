cbad37a74802955f291b4501d8aaebf09ab3a320
Hang Shi <hangshiisi@gmail.com>
2017-09-22 22:49:43 -0700

    tweet sentiment

--- dci/assignment1/tweet_sentiment.py
+++ dci/assignment1/tweet_sentiment.py
@@ -1,17 +1,35 @@
 import sys
+import json 
 
-def hw():
-    print 'Hello, world!'
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def lines_in(f):
+    '''parse sentiment file, returns dictionary''' 
+    scores = {} 
+    with open(f) as fp: 
+        for line in fp: 
+            term, score = line.split("\t") 
+            scores[term] = int(score) 
+        return scores 
+
+def tweet_score(tweet, scores): 
+    '''returns the score for a tweet or 0 if it's not in file''' 
+    return sum(scores.get(word, 0) for word in tweet.split())    
+
+def lines_out(f, scores):
+    '''Calculate scores for all tweets in tweet file'''
+    with open(f) as fp:  
+        tweets = (json.loads(line).get('text','') for line in fp) 
+        return [tweet_score(tweet, scores) for tweet in tweets]  
+
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    sent_file = sys.argv[1]
+    tweet_file = sys.argv[2]
+    scores = lines_in(sent_file)
+    senti_scores = lines_out(tweet_file, scores)
+    print(senti_scores) 
+    sys.stdout.writelines('{0}.0\n'.format(score) for score in senti_scores) 
+
 
 if __name__ == '__main__':
     main()
